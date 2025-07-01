2006fd21c191eafd43f89532cb1654ebb8d65bff
ShadowwMurloc <dhwork2004@gmail.com>
2025-07-01 05:59:24 +0200

    grid adjustment in progress

--- AutoLink/obj/Debug/net9.0-ios/iossimulator-x64/AutoLink.AssemblyInfo.cs
+++ AutoLink/obj/Debug/net9.0-ios/iossimulator-x64/AutoLink.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e3ef0bb6ef2477f0d0bc3a77b034a136da3f3e40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f054cd209aa83e4e9a4121841faad94d69fd5419")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
